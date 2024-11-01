7d85880c28505da1c34f6508a4b55e7004b301a4
Luna1808 <lunsbc18@gmail.com>
2024-11-01 15:14:05 -0600

    Se arregla parte de las listas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from './Components/Login';
-import Listado from './Components/Listado';
 import RegisterPage from './Pages/RegisterPage';
 import Navbar from './Components/Navbar';
+import ListPage from './Pages/ListPage';
 
 export default function App(){
   return (
     <Router>
     <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Listado" element={<Listado />} />
+        <Route path="/ListPage" element={<ListPage />} />
         <Route path="/RegisterPage" element={<RegisterPage />} />
         <Route path="/Navbar" element={<Navbar />} />
 
---
+++ Frontend/src/Pages/ListPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Listado from "../Components/Listado";
+import Navbar from "../Components/Navbar";
+
+const ListPage = () => {
+  return (
+    <>
+      <Navbar />
+      <Listado/>
+    </>
+  );
+};
+
+export default ListPage;
