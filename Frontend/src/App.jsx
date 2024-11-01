2c3ae719b468d5253293f8e7c28322821ff5f2de
Angelica <angelicacb6392@gmail.com>
2024-11-01 10:53:43 -0600

    Prueba de lista

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from './Components/Login';
+import Listado from './Components/Listado';
 
 export default function App(){
   return (
     <Router>
     <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/Listado" element={<Listado />} />
+
     </Routes>
 </Router>
   );
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         'Purple': '#8b5cf6',
         'violet': '#3a2e60',
         'blue': '#426bb2',
+        'light-blue': '#67e8f9' 
       }
     },
   },
