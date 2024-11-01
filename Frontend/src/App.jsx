66b67e017bb123b773803acd552f83ac713c0490
Luna1808 <lunsbc18@gmail.com>
2024-11-01 15:53:31 -0600

    Ajustes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,12 +5,13 @@ import Login from './Components/Login';
 import RegisterPage from './Pages/RegisterPage';
 import Navbar from './Components/Navbar';
 import ListPage from './Pages/ListPage';
+import { LoginPage } from './Pages/LoginPage';
 
 export default function App(){
   return (
     <Router>
     <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/ListPage" element={<ListPage />} />
         <Route path="/RegisterPage" element={<RegisterPage />} />
         <Route path="/Navbar" element={<Navbar />} />
--- Frontend/src/Pages/ListPage.jsx
+++ Frontend/src/Pages/ListPage.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Listado from "../Components/Listado";
 import Navbar from "../Components/Navbar";
+import Sidebar from "../Components/Sidebar";
+
 
 const ListPage = () => {
   return (
     <>
+      <div className="bg-gray-50 flex-col min-h-screen">
       <Navbar />
-      <Listado/>
+      <div className="flex flex-1"> 
+        <div className="w-36"> 
+          <Sidebar />
+        </div>
+        <div className="flex-1 flex justify-center mr-8"> 
+          <Listado/>
+        </div>
+      </div>
+    </div>
     </>
   );
 };
