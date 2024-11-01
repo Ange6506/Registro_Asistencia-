f3d8bdc76d684d1be81cd89c296b9b2fce9c654f
Angelica <160287988+Ange6506@users.noreply.github.com>
2024-11-01 11:01:11 -0500

    Navbar-Idea de registro

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from './Components/Login';
+import RegisterPage from './Pages/RegisterPage';
+import Navbar from './Components/Navbar';
 
 export default function App(){
   return (
     <Router>
     <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/RegisterPage" element={<RegisterPage />} />
+        <Route path="/Navbar" element={<Navbar />} />
+
     </Routes>
 </Router>
   );
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         'Purple': '#8b5cf6',
         'violet': '#3a2e60',
         'blue': '#426bb2',
+        'blue-gray': {
+          900: '#1f2937',
+        },
       }
     },
   },
