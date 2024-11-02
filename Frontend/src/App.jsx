3ac77b4a6b4998cbc9427c8d80d12c2b7c5d3d35
Angelica <160287988+Ange6506@users.noreply.github.com>
2024-11-02 08:42:16 -0500

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from './Components/Login';
 import RegisterPage from './Pages/RegisterPage';
-import Navbar from './Components/Navbar';
 
 export default function App(){
   return (
@@ -11,8 +10,6 @@ export default function App(){
     <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/RegisterPage" element={<RegisterPage />} />
-        <Route path="/Navbar" element={<Navbar />} />
-
     </Routes>
 </Router>
   );
