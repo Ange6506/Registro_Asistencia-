4268f0f8744434eaeb237ffd3ce26a26ad22f423
Angelica <160287988+Ange6506@users.noreply.github.com>
2024-11-07 07:52:34 -0500

    Organizar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Login from "./Components/Login";
 import RegisterPage from "./Pages/RegisterPage";
-import Navbar from "./Components/Dashboard/DashboardComponents/Navbar";
 import { LoginPage } from "./Pages/LoginPage";
-import ListPage from "./Pages/ListPage";
-import Home from "./Components/Home";
 import { HomePage } from "./Pages/HomePage";
 
 export default function App() {
@@ -14,9 +10,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/ListPage" element={<ListPage />} />
         <Route path="/RegisterPage" element={<RegisterPage />} />
-        <Route path="/Home" element={<Home />} />
         <Route path="/HomePage" element={<HomePage />} />
       </Routes>
     </Router>
--- Frontend/src/Pages/RegisterPage.jsx
+++ Frontend/src/Pages/RegisterPage.jsx
@@ -1,21 +1,11 @@
 import React from "react";
-import Register from "../Components/Register";
-import Navbar from "../Components/Dashboard/DashboardComponents/Navbar";
-import Sidebar from "../Components/Sidebar";
+import { Dashboard } from "../Components/Dashboard/Dashboard";
 
 const RegisterPage = () => {
   return (
-    <div className="bg-gray-50 flex-col min-h-screen">
-      <Navbar />
-      <div className="flex flex-1">
-        <div className="w-36">
-          <Sidebar />
-        </div>
-        <div className="flex-1 flex justify-center mr-8">
-          <Register />
-        </div>
-      </div>
-    </div>
+    <>
+      <Dashboard />
+    </>
   );
 };
 
