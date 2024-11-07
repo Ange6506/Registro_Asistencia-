750be4dff8e35267062366e3aa3739024494f8a3
Luna1808 <lunsbc18@gmail.com>
2024-11-07 07:47:53 -0600

    Se realizan cambios

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import HomeImg from '../assets/Img/Home.svg'
-import coty from '../assets/Img/coty.svg'
-import react from '../assets/Img/react.svg'
-import nodejs from '../assets/Img/nodejs.svg'
-import tailwind from '../assets/Img/tailwind.svg'
+import React, { useState } from "react";
+import HomeImg from "../assets/Img/Home.svg";
+import coty from "../assets/Img/coty.svg";
+import react from "../assets/Img/react.svg";
+import nodejs from "../assets/Img/nodejs.svg";
+import tailwind from "../assets/Img/tailwind.svg";
 
 const Home = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -15,11 +15,10 @@ const Home = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission logic (e.g., send email)
-    console.log('Email submitted:', email);
+    console.log("Email submitted:", email);
   };
 
   return (
-    
     <div className="pt-32 md:py-12 xl:container m-auto px-6 md:px-12">
       <div
         aria-hidden="true"
@@ -28,11 +27,12 @@ const Home = () => {
       <div className="relative lg:flex lg:items-center lg:gap-12">
         <div className="text-center lg:text-left md:mt-12 lg:mt-0 sm:w-10/12 md:w-2/3 sm:mx-auto lg:mr-auto lg:w-6/12">
           <h1 className="text-gray-900 font-bold text-4xl md:text-6xl lg:text-5xl xl:text-6xl dark:text-white">
-          Control de Asistencia para Estudiantes
-          {' '}
+            Control de Asistencia para Estudiantes{" "}
           </h1>
           <p className="mt-8 text-gray-600 dark:text-gray-300">
- Este sistema de software libre está diseñado específicamente para la Universidad en la Clínica Nueva Rafael Uribe Uribe, ofreciendo seguridad y facilidad en el control de asistencia.
+            Este sistema de software libre está diseñado específicamente para la
+            Universidad en la Clínica Nueva Rafael Uribe Uribe, ofreciendo
+            seguridad y facilidad en el control de asistencia.
           </p>
           <div>
             <form onSubmit={handleSubmit} className="w-full mt-12">
@@ -73,7 +73,11 @@ const Home = () => {
                       stroke="currentColor"
                       strokeWidth="2"
                     >
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
                     </svg>
                   </button>
                 </div>
@@ -81,15 +85,30 @@ const Home = () => {
             </form>
           </div>
           <div className="mt-12 flex flex-wrap gap-6 justify-center lg:justify-between items-center">
-  <img src={tailwind} className="h-6 sm:h-8 md:h-10 lg:h-12 w-auto" alt="Tailwind Logo" />
-  <img src={react} className="h-6 sm:h-8 md:h-10 lg:h-12 w-auto" alt="React Logo" />
-  <img src={coty} className="h-6 sm:h-8 md:h-10 lg:h-12 w-auto" alt="Coty Logo" />
-  <img src={nodejs} className="h-6 sm:h-8 md:h-10 lg:h-12 w-auto" alt="Node.js Logo" />
-</div>
-
+            <img
+              src={tailwind}
+              className="h-6 sm:h-8 md:h-10 lg:h-12 w-auto"
+              alt="Tailwind Logo"
+            />
+            <img
+              src={react}
+              className="h-6 sm:h-8 md:h-10 lg:h-12 w-auto"
+              alt="React Logo"
+            />
+            <img
+              src={coty}
+              className="h-6 sm:h-8 md:h-10 lg:h-12 w-auto"
+              alt="Coty Logo"
+            />
+            <img
+              src={nodejs}
+              className="h-6 sm:h-8 md:h-10 lg:h-12 w-auto"
+              alt="Node.js Logo"
+            />
+          </div>
         </div>
         <div className="overflow-hidden w-full lg:w-7/12 lg:-mr-16">
-          <img src={HomeImg} alt="HomeImg"  height="" width="" />
+          <img src={HomeImg} alt="HomeImg" height="" width="" />
         </div>
       </div>
     </div>
