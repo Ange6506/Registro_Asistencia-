488a6ccb0e3b1eaf307b5f0e1c9eb16f3c301b64
Angelica <angelicacb6392@gmail.com>
2025-01-27 16:19:25 -0500

    Update user.routes.js

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
+
 //estudiantes
 const getEstudiantes  = require("../controllers/student/getEstudiantes");
 const addEstudiante  = require("../controllers/student/addEstudiante");
 const deleteEstudiante  = require("../controllers/student/deleteEstudiante");
 const addHuella  = require("../controllers/student/addHuella");
-
 
 //Asistencia
 const getAsistencia  = require("../controllers/Attendance/getAsistencia");
@@ -27,22 +27,30 @@ const  getRoles  = require("../controllers/users/getRoles");
 const  updateUser  = require("../controllers/users/updateUser");
 const addUsers = require("../controllers/users/addUser");
 
+
+//router post
 router.post("/add_user", addUsers.addUser);
 router.post("/add_Asistencia", addAsistencia.addAsistencia);
+router.post("/login", loginUser.loginUser);
 router.post("/add_Huella", addHuella.addHuella);
+router.post("/addPrograma", addPrograma.addPrograma);
+
+//router get
 router.get("/get_estudiante", getEstudiantes.getEstudiantes);
 router.get("/getAsistencia", getAsistencia.getAsistencia);
-router.post("/login", loginUser.loginUser);
-router.delete("/deletestudent/:identificacion", deleteEstudiante.deleteEstudiante);
 router.get("/getPrograma", getPrograma.getPrograma);
-router.post("/addPrograma", addPrograma.addPrograma);
-router.put("/update_programa/:id", updatePrograma.updatePrograma);
-router.delete("/deletePrograma/:id_programa", deleteProgram.deleteProgram);
-router.put("/updateSemestre/:id_semestre", updateSemestre.updateSemestre);
-router.post("/addEstudiante", addEstudiante.addEstudiante);
 router.get("/getUser", getUser.getUser);
 router.get("/getRoles", getRoles.getRoles);
+
+//router put
+router.put("/update_programa/:id", updatePrograma.updatePrograma);
+router.put("/updateSemestre/:id_semestre", updateSemestre.updateSemestre);
+router.post("/addEstudiante", addEstudiante.addEstudiante);
 router.put("/updateUser/:id", updateUser.updateUser);
+
+//router delete
+router.delete("/deletestudent/:identificacion", deleteEstudiante.deleteEstudiante);
+router.delete("/deletePrograma/:id_programa", deleteProgram.deleteProgram);
 
 
 module.exports = router;
